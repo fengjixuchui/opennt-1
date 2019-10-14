dfc0b981353a2473a0ef50737fb26dd4b7458188
smartmaster <smart.master@live.com>
2019-10-14 23:24:32 +0800

    Signed-off-by: smartmaster <smart.master@live.com>

--- nt/private/developr/x86chk/x86chk.cmd
+++ nt/private/developr/x86chk/x86chk.cmd
@@ -7,12 +7,17 @@ set _NT386TREE=%BINARIES%\nt
 set _CAIRO386TREE=%CAIROBINS%\nt
 
 REM Debug Information
+REM https://www.osronline.com/article.cfm%5Eid=68.htm
+REM https://reverseengineering.stackexchange.com/questions/17288/is-all-debug-information-of-vs-compiled-pe-files-contained-in-an-external-pdb-fi
+REM https://blog.csdn.net/zhou191954/article/details/38174793
+REM http://www.debuginfo.com/articles/gendebuginfo.html
 set NTDEBUG=ntsd
-set NTDEBUGTYPE=both
+set USE_PDB=1
+set NTDEBUGTYPE=windbg
 
 REM Optimization Settings
-set 386_OPTIMIZATION=/Od /Oi- /Zi
+set 386_OPTIMIZATION=/Zi /Od /Oi-
 
 REM Build Configurations
 set NT_UP=0
-REM set DEBUG_CRTS=1
+set DEBUG_CRTS=1
