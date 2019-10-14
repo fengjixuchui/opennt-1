de1dc781689547751557bf0a04ec89ca1d9e6b4c
smartmaster <smart.master@live.com>
2019-10-14 18:53:45 +0800

    windbg 6.6 can support windows nt 4 kernel debug Signed-off-by: smartmaster <smart.master@live.com>

--- windbg-nt4-esx-vm.cmd
+++ windbg-nt4-esx-vm.cmd
@@ -2,6 +2,6 @@ set volume_root=D:
 set wrk=%~dp0
 set windbgargs=-k com:port=com1,baud=115200 -srcpath %wrk%nt\private
 set _NT_SYMBOL_PATH=%wrk%binaries\nt\Symbols;%CORE_ROOT%;%CORE_ROOT%\pdb;%volume_root%\pdb;srv*%volume_root%\pdb*http://msdl.microsoft.com/download/symbols
-rem "E:\WinDDK\7600.16385.1\Debuggers\windbg.exe" %windbgargs%
+rem start ""  "E:\WinDDK\7600.16385.1\Debuggers\windbg.exe" %windbgargs%
 start "" "E:\windbg6.6\windbg.exe" %windbgargs%
 
